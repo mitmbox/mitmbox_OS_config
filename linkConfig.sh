246cc928afdde539493a9db2d256076900306a2d
mahrla <root@bananapi.(none)>
2015-11-29 12:21:55 +0000

    adding os config

---
+++ linkConfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+/bin/ln -f -s /root/mitmbox/os_configuration/config/interfaces /etc/network/interfaces
+/bin/ln -f -s /root/mitmbox/os_configuration/config/swconfig /etc/network/if-pre-up.d/swconfig
+/bin/ln -f -s /root/mitmbox/os_configuration/config/renameInterfaces /etc/network/if-up.d/renameInterfaces
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/hostapd /usr/sbin/hostapd
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/hostapd_cli /usr/sbin/hostapd_cli
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/hostapd.conf /etc/hostapd.conf
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/default/hostapd /etc/default/hostapd
