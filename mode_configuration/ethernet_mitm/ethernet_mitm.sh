b0217a4d9f0bbc7de0652c3209d785ee65b77997
mahrla <markus.mahrla@gmx.de>
2015-12-26 12:28:30 +0000

    changed naming

---
+++ mode_configuration/ethernet_mitm/ethernet_mitm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# network ports
+/root/new/mitmbox_config/mode_configuration/ethernet_mitm/swconfig-mitm
+
+# processes
+killall hostapd
+killall wpa_supplicant
+killall dnsmasq
+killall dhclient
+
+# ip fordwarding
+sysctl -w net.ipv4.ip_forward=0
+
+# routing
+route del -net 0.0.0.0 tun0
+route add net 10.0.0.0 netmask 255.255.255.0 eth0
+
